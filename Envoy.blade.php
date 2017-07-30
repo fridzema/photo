ab24cabb62a1d017e739f15674553005602c40e9
Robert Fridzema <robertfridzema@Roberts-MacBook-Pro.local>
2017-07-30 18:25:28 +0200

    .

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,69 +1,84 @@
 @servers(['localhost' => '127.0.0.1', 'fridzema' => 'root@188.226.137.26'])
-
   @setup
       $path = "/var/www/html";
+      require __DIR__.'/vendor/autoload.php';
+			(new \Dotenv\Dotenv(__DIR__, '.env'))->load();
+			$silent_output = false;
+
+			$path = "/var/www/html";
+			$repositoryName = "photo";
+			$repositoryUser = "fridzema";
+
+			function logMessage($message) {
+				return "echo '\033[1;32m" .$message. "\033[0m';\n";
+			}
   @endsetup
 
-  @task('set_down', ['on' => 'fridzema'])
-    cd {{ $path }}
-    php artisan down --message="Upgrading Website" --retry=20
+  @macro('deploy_clean_server')
+    clean_current_dir
+    git
+    fix_permissions
+    copy_env
+    composer
+    migrate_seed
+    optimize
+    restart_services
+  @endmacro
+
+  @task('clean_current_dir', ['on' => 'fridzema'])
+  	{{ logMessage("👋  Directory cleanup...") }}
+    cd {{ $path }};
+    rm -rf *;
   @endtask
 
-  @task('git_composer', ['on' => 'fridzema'])
-    cd {{ $path }}
-    git fetch origin master
-    git reset --hard FETCH_HEAD
-    composer install
+  @task('git', ['on' => 'fridzema'])
+  	{{ logMessage("⛓  Cloning the repository ".$repositoryUser."/".$repositoryName."...") }}
+    cd {{ $path }};
+    git clone --depth 1 https://github.com/{{$repositoryUser}}/{{$repositoryName}}.git --quiet;
+  @endtask
+
+  @task('fix_permissions', ['on' => 'fridzema'])
+  	{{ logMessage("🔓  Fixing file permissions...") }}
+  	cd {{ $path }};
+    chmod 755 -R {{$repositoryName}};
+
+    cd {{ $path }}/{{$repositoryName}};
+    chmod -R o+w storage;
   @endtask
 
   @task('copy_env', ['on' => 'fridzema'])
-    cd {{ $path }}
-    rm -rf .env
-    cp .env.production .env
+  	{{ logMessage("⚠️  Copy the env production file...") }}
+    cd {{ $path }}/{{$repositoryName}};
+    cp .env.production .env;
   @endtask
 
-  @task('cache', ['on' => 'fridzema'])
-    cd {{ $path }}
-    php artisan optimize
-    php artisan cache:clear
-    php artisan view:clear
-    php artisan config:cache
-    php artisan route:cache
-    php artisan responsecache:flush
+	@task('composer', ['on' => 'fridzema'])
+		{{ logMessage("🚚  Running composer...") }}
+		cd {{ $path }}/{{$repositoryName}};
+		composer install --prefer-dist --no-scripts --no-dev -o;
+	@endtask
+
+  @task('optimize', ['on' => 'fridzema'])
+		{{ logMessage("🏎  Speed things up a bit up...") }}
+    cd {{ $path }}/{{$repositoryName}};
+    php artisan clear-compiled -q;
+		php artisan optimize -q;
+    php artisan cache:clear -q;
+    php artisan view:clear -q;
+    php artisan route:cache -q;
+    php artisan config:cache -q;
   @endtask
 
-  @task('set_up', ['on' => 'fridzema'])
-    cd {{ $path }}
-    php artisan up
+  @task('migrate_seed', ['on' => 'fridzema'])
+  	{{ logMessage("⚙️  Build and fill the database...") }}
+    cd {{ $path }}/{{$repositoryName}};
+		php artisan migrate:refresh --seed --force -q;
   @endtask
 
-  @task('local_update', ['on' => 'localhost'])
-    npm run production
-    git add .
-    git commit -m "Deploy to server"
-    git push
+  @task('restart_services', ['on' => 'fridzema'])
+  	{{ logMessage("🛁  Keep it fresh...") }}
+  	service mysql --full-restart;
+    service nginx --full-restart;
+    service php7.0-fpm --full-restart;
+   	service redis-server --full-restart;
   @endtask
-
-
-  @task('restart_servers', ['on' => 'fridzema'])
-    service nginx restart
-    service php7.0-fpm restart
-  @endtask
-
-  @macro('deploy_all')
-    local_update
-    set_down
-    git_composer
-    copy_env
-    set_up
-    cache
-    restart_servers
-  @endmacro
-
-  @macro('upload')
-    set_down
-    git_composer
-    copy_env
-    set_up
-    cache
-  @endmacro
---
+++ Envoyb186626da71ae100a770e48fd99c36fa.php
@@ -0,0 +1,88 @@
+<?php $X = isset($X) ? $X : null; ?>
+<?php $message = isset($message) ? $message : null; ?>
+<?php $repositoryUser = isset($repositoryUser) ? $repositoryUser : null; ?>
+<?php $repositoryName = isset($repositoryName) ? $repositoryName : null; ?>
+<?php $silent_output = isset($silent_output) ? $silent_output : null; ?>
+<?php $path = isset($path) ? $path : null; ?>
+<?php $__container->servers(['localhost' => '127.0.0.1', 'fridzema' => 'root@188.226.137.26']); ?>
+  <?php
+      $path = "/var/www/html";
+      require __DIR__.'/vendor/autoload.php';
+			(new \Dotenv\Dotenv(__DIR__, '.env'))->load();
+			$silent_output = false;
+
+			$path = "/var/www/html";
+			$repositoryName = "photo";
+			$repositoryUser = "fridzema";
+
+			function logMessage($message) {
+				return "echo '\033[1;32m" .$message. "\033[0m';\n";
+				return "echo 'while true; do for X in '-' '/' '|' '\'; do echo -en "\b$X"; sleep 0.1; done; done '";
+			}
+  ?>
+
+  <?php $__container->startMacro('deploy_clean_server'); ?>
+    clean_current_dir
+    git
+    copy_env
+    composer
+    migrate_seed
+    optimize
+    restart_services
+  <?php $__container->endMacro(); ?>
+
+  <?php $__container->startTask('clean_current_dir', ['on' => 'fridzema']); ?>
+  	<?php echo logMessage("👋  Directory cleanup..."); ?>
+
+    cd <?php echo $path; ?>;
+    rm -rf *;
+  <?php $__container->endTask(); ?>
+
+  <?php $__container->startTask('git', ['on' => 'fridzema']); ?>
+  	<?php echo logMessage("⛓  Cloning the repository ".$repositoryUser."/".$repositoryName."..."); ?>
+
+    cd <?php echo $path; ?>;
+    git clone --depth 1 https://github.com/<?php echo $repositoryUser; ?>/<?php echo $repositoryName; ?>.git --quiet;
+  <?php $__container->endTask(); ?>
+
+  <?php $__container->startTask('copy_env', ['on' => 'fridzema']); ?>
+  	<?php echo logMessage("⚠️  Copy the env production file..."); ?>
+
+    cd <?php echo $path; ?>/<?php echo $repositoryName; ?>;
+    cp .env.production .env;
+  <?php $__container->endTask(); ?>
+
+	<?php $__container->startTask('composer', ['on' => 'fridzema']); ?>
+		<?php echo logMessage("🚚  Running composer..."); ?>
+
+		cd <?php echo $path; ?>/<?php echo $repositoryName; ?>;
+		composer install --prefer-dist --no-scripts --no-dev -q -o;
+	<?php $__container->endTask(); ?>
+
+  <?php $__container->startTask('optimize', ['on' => 'fridzema']); ?>
+		<?php echo logMessage("🏎  Speed things up a bit up..."); ?>
+
+    cd <?php echo $path; ?>/<?php echo $repositoryName; ?>;
+    php artisan clear-compiled -q;
+		php artisan optimize -q;
+    php artisan cache:clear -q;
+    php artisan view:clear -q;
+    php artisan route:cache -q;
+    php artisan config:cache -q;
+  <?php $__container->endTask(); ?>
+
+  <?php $__container->startTask('migrate_seed', ['on' => 'fridzema']); ?>
+  	<?php echo logMessage("⚙️  Build and fill the database..."); ?>
+
+    cd <?php echo $path; ?>/<?php echo $repositoryName; ?>;
+		php artisan migrate:refresh --seed --force -q;
+  <?php $__container->endTask(); ?>
+
+  <?php $__container->startTask('restart_services', ['on' => 'fridzema']); ?>
+  	<?php echo logMessage("🛁  Keep it fresh..."); ?>
+
+  	service mysql --full-restart;
+    service nginx --full-restart;
+    service php7.0-fpm --full-restart;
+   	service redis-server --full-restart;
+  <?php $__container->endTask(); ?>
