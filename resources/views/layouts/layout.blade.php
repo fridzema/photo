6517e4eff4a6027acc41fe424c7e070bdf3193c7
Robert Fridzema <robertfridzema@Roberts-MacBook-Pro.local>
2017-08-10 21:29:03 +0200

    .

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <title>Robert Fridzema - Rotterdam</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="description" content="Robert Fridzema, Photography Rotterdam">
       <meta name="keywords" content="robert, fridzema, rotterdam, photo, photgraph, camera">
       <style>{!!file_get_contents('css/app.css')!!}</style>
