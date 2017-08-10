93570f5db3fc55b704ebed38aaf465795b47c820
rf <rf@Mac-Pro.local>
2017-08-10 13:13:05 +0200

    .

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -11,5 +11,10 @@
     <body>
     	<h1><a href="mailto:fridzema@gmail.com" rel="author">Robert Fridzema</a></h1>
       @yield('content')
+      <script>
+          window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+          ga('create','UA-104407435-1','auto');ga('send','pageview')
+      </script>
+      <script src="https://www.google-analytics.com/analytics.js" async defer></script>
     </body>
 </html>
