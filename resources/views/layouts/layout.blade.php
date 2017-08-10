e61f80a0754af1845597d8086ffaed2296410a41
rf <rf@Mac-Pro.local>
2017-08-10 13:24:38 +0200

    .

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -11,6 +11,7 @@
     <body>
     	<h1><a href="mailto:fridzema@gmail.com" rel="author">Robert Fridzema</a></h1>
       @yield('content')
+
 			<script>
 			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
