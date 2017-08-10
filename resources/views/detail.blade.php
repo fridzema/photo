34e3b2d96e4db0c25d908aa49deac1c97e4e5681
Robert Fridzema <robertfridzema@Roberts-MacBook-Pro.local>
2017-08-10 22:12:58 +0200

    .

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.layout')
 
 @section('content')
-<img src="https://static.fridzema.com{{$photo->getMedia('images')->first()->getUrl('large')}}"  />
+<img src="https://static.fridzema.com{{$photo->getMedia('images')->first()->getUrl('large')}}" alt="{{ str_random(3) }}" width="1800"  />
 @endsection
