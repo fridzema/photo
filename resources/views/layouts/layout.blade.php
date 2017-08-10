2f7ab0ccb140e231e1dd2aa6242f73d961a69415
rf <rf@Mac-Pro.local>
2017-08-10 13:36:10 +0200

    .

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,11 +7,6 @@
       <meta name="description" content="Robert Fridzema, Photography Rotterdam">
       <meta name="keywords" content="robert, fridzema, rotterdam, photo, photgraph, camera">
       <style>{!!file_get_contents('css/app.css')!!}</style>
-    </head>
-    <body>
-    	<h1><a href="mailto:fridzema@gmail.com" rel="author">Robert Fridzema</a></h1>
-      @yield('content')
-
 			<script>
 			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -21,5 +16,9 @@
 			  ga('create', 'UA-104407435-1', 'auto');
 			  ga('send', 'pageview');
 			</script>
+    </head>
+    <body>
+    	<h1><a href="mailto:fridzema@gmail.com" rel="author">Robert Fridzema</a></h1>
+      @yield('content')
     </body>
 </html>
