166454205a76c9a03bde021f755f7dc88c31cbca
Robert Fridzema <robertfridzema@Roberts-MacBook-Pro.local>
2017-08-10 21:31:51 +0200

    .

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,18 +6,14 @@
       <meta name="description" content="Robert Fridzema, Photography Rotterdam">
       <meta name="keywords" content="robert, fridzema, rotterdam, photo, photgraph, camera">
       <style>{!!file_get_contents('css/app.css')!!}</style>
-			<script>
-			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-			  ga('create', 'UA-104407435-1', 'auto');
-			  ga('send', 'pageview');
-			</script>
     </head>
     <body>
     	<h1><a href="mailto:fridzema@gmail.com" rel="author">Robert Fridzema</a></h1>
       @yield('content')
+      <script>
+          window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+          ga('create','UA-104407435-1','auto');ga('send','pageview')
+      </script>
+      <script src="https://www.google-analytics.com/analytics.js" async defer></script>
     </body>
 </html>
